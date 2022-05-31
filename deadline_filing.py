f4e99f49ed1033b082fa0f774346d9a022c3a53f
Djos1980 <106580847+Djos1980@users.noreply.github.com>
2022-05-31 10:29:55 +0300

    Add files via upload

---
+++ deadline_filing.py
@@ -0,0 +1,27 @@
+# Import date modules
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+# Define type of IP and calculate filing deadline
+IP = input("Please enter type of patent (PCT, Direct, EP Validation): ")
+# Define the earliest priority date based on client input
+if IP == "PCT":
+    priority = input("Please enter the earliest priority date YYYY-MM-DD: ")
+    year, month, day = map(int, priority.split('-'))  # split response and map
+    priority = date(year, month, day)  # convert response to date
+    deadline = priority + relativedelta(months=30)
+    print('Filing deadline is ' + str(deadline))
+
+elif IP == "Direct":
+    priority = input("Please enter the earliest priority date YYYY-MM-DD: ")
+    year, month, day = map(int, priority.split('-'))  # split response and map
+    priority = date(year, month, day)
+    deadline = priority + relativedelta(months=12)
+    print('Filing deadline is ' + str(deadline))
+
+elif IP == "EP Validation":
+    priority = input("Please enter the grant date YYYY-MM-DD: ")
+    year, month, day = map(int, priority.split('-'))  # split response and map
+    priority = date(year, month, day)
+    deadline = priority + relativedelta(months=3)
+    print('Validation deadline is ' + str(deadline))
